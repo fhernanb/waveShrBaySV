caaea32dbcc527fb0d4af0f58b9f4078c8cae138
omarrios81 <60475030+omarrios81@users.noreply.github.com>
2025-02-12 11:30:33 -0500

    Update plWav1j.R

--- R/plWav1j.R
+++ R/plWav1j.R
@@ -1,21 +1,17 @@
 #' plWav1j
 #'
-#' Esta funcion calcula bla bla bla.
+#' Esta funcion elimina el ruido aditivo de una serie de observaciones por medio de un algoritmo bayesiano basado en el aprendizaje de partículas.
+#' La función utiliza un método shrinkage bayesiano basado en particle learning los coeficientes de la transformación wavelet de las observaciones. 
 #'
-#' @param dat bla bla bla, Omar debe completar esto.
-#' @param filter.number bla bla bla, Omar debe completar esto.
-#' @param family bla bla bla, Omar debe completar esto.
-#' @param a bla bla bla, Omar debe completar esto.
-#' @param b bla bla bla, Omar debe completar esto.
-#' @param bet bla bla bla, Omar debe completar esto.
-#' @param nu0  bla bla bla, Omar debe completar esto.
-#' @param lamb0  bla bla bla, Omar debe completar esto.
-#' @param M  bla bla bla, Omar debe completar esto.
-#' @param Ne  bla bla bla, Omar debe completar esto.
-#' @param j0  bla bla bla, Omar debe completar esto.
-#' @param plot.EMPL  bla bla bla, Omar debe completar esto.
+#' @param dat Serie de observaciones a ingresar a las que se le debe eliminar el ruido aditivo.
+#' @param filter.number Parámetro de la transformación wavelet que indica en número de momentos de desvanecimiento.
+#' @param family Familia de la transformación wavelet c('DaubExPhase','DaubLeAsymm','Coiflets',...).
+#' @param M  Número pasos programados en el proceso de maximización del algoritmo.
+#' @param Ne  Número de partículas el proceso particle learning.
+#' @param j0  Nivel de resolución de la transformación wavelet.
+#' @param plot.EMPL Gráfica de comparación entre la serie de observaciones original y serie libre de ruido.
 #'
-#' @return Esta funcion retorna bla bla bla, Omar debe completar esto..
+#' @return Esta funcion retorna la serie de observaciones libre de ruido al nivel de resolución especificado.
 #'
 #' @example examples/examples_plWav1j.R
 #'
@@ -25,9 +21,8 @@
 #' @importFrom stats mad rgamma
 #' @importFrom graphics par
 #' @importFrom wavethresh putD accessD wr accessC nlevelsWT
-plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10, bet = 1,
-                  nu0 = 5,lamb0 = 10, M = 10, Ne, j0 = nlevelsWT(vw), plot.EMPL = FALSE){
-  #require(wavethresh) Omar, no use esta notacion
+plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", M = 10, Ne = 200, j0 = nlevelsWT(vw), plot.EMPL = FALSE){
+  #a = 10; b = 10; bet = 1; nu0 = 5; lamb0 = 10
   set.seed(1321)
   vw <- wavethresh::wd(dat,filter.number=filter.number,family=family)
   sigma<-mad(accessD(vw,level=(nlevelsWT(vw)-1)))/0.6745
@@ -36,8 +31,8 @@ plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10
     coefthr<-accessD(vw,level=j)
     pr1a[(2^j:(2^(j+1)-1))]<-rev(coefthr)
   }
-  a<-a
-  b<-b*sigma^2
+  a<-10
+  b<-10*sigma^2
   Oj<-NULL
   tau12<-1/rgamma(1,a+3,b+10)
   Psi<-NULL
@@ -46,7 +41,7 @@ plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10
     if(j==0){
       pr[1]<-0.999999
     }else{
-      pr[j+1]<-2^(-j*bet)
+      pr[j+1]<-2^(-j*1)
     }
     coefthr<-accessD(vw,level=j)
     Oj[(2^j:(2^(j+1)-1))]<-(sqrt(sigma^2+tau12)/sigma)*((1-pr[j+1])/pr[j+1])*exp(-0.5*(rev(coefthr)^2/sigma^2)*(tau12/(tau12+sigma^2)))
@@ -58,12 +53,10 @@ plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10
     c0[j+1]<-max(0,sum(Psi[(2^j:(2^(j+1)-1))]*pr1a[(2^j:(2^(j+1)-1))]^2)/(sigma^2*sum(Psi[(2^j:(2^(j+1)-1))]))-1)
   }
   d<-dx<-matrix(0,ncol=length(vw$D),nrow=Ne)
-  nu0<-nu0
-  lamb0<-lamb0
+  nu0<-5
+  lamb0<-10
   S<-cbind(rep(nu0/2,Ne),rep(lamb0*nu0/2,Ne))
   sig1<-1/rgamma(Ne,nu0/2,lamb0*nu0/2)
-  #prF<-pi0
-  #stF<-c0
   Psi1F<-matrix(0,ncol=length(vw$D),nrow = Ne)
   Oj1F<-matrix(0,ncol=length(vw$D),nrow = Ne)
   pr1b<-NULL
@@ -89,9 +82,7 @@ plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10
         S[,2]<-S[k,2]+(pr1b[i]-d[,i])^2/2  # pag 112 UPCcourse-handouts.pdf
         sig1<-1/rgamma(Ne,S[,1],S[,2])
         d[,i]<-rnorm(Ne,Psi1F[,i]*stF[m-1,j+1]/(1+stF[m-1,j+1])*pr1b[i],sd=sqrt(Psi1F[,i]*sig1*stF[m-1,j+1]/(1+stF[m-1,j+1])))
-        #dx[,i]<-sample(d[,i],size=Ne,replace=TRUE,prob=w)
         Psi1p[i]<-sample(Psi1F[,i],size=1,replace=FALSE,prob=rep(1/Ne,Ne))
-        #Psi1p[i]<-sum(Psi1F[,i]/Ne)
         sig1p<-mean(sig1)
       }
       #E-step
@@ -104,10 +95,10 @@ plWav1j<-function(dat, filter.number = 4, family = "DaubLeAsymm", a = 10, b = 10
   for(i in 1:length(pr1b)){
     Ex5F1[i]<-mean(d[,i])}
   for (j in 0:(j0-1)){
-    bayes3<-putD(bayes3,level=j,v=rev(Ex5F1[(2^j:(2^(j+1)-1))]))
+    bayes3<-wavethresh::putD(bayes3,level=j,v=rev(Ex5F1[(2^j:(2^(j+1)-1))]))
   }
-  bayesrec5<-wr(bayes3)
-  D<-accessC(vw,level=0)
+  bayesrec5<-wavethresh::wr(bayes3)
+  D<-wavethresh::accessC(vw,level=0)
   if (plot.EMPL == TRUE) {
     x <- seq(1, length(dat))/length(dat)
     par(mfrow = c(1, 2))
